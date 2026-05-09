516959e032d037eb82a4a541e583c1405add9e09
Sadathy <118998237+Sadathy@users.noreply.github.com>
2026-05-09 20:48:12 +0100

    Added fuel
    Limited plotted orders to the maximum movement the ship can achieve in a given turn. indicator snaps to max range and all updated dynamically while plotting / between turns

--- wild-jam-93-project/Scripts/Player/player_ship.gd
+++ wild-jam-93-project/Scripts/Player/player_ship.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed: float = 5.0
+@export var speed: float = 100.0
 @export var DEFAULT_PLOT_COOLDOWN: float = 0.15
 
 @onready var sprite: Sprite2D = %Sprite
@@ -15,6 +15,7 @@ const TARGET_LINE = preload("res://Scenes/UI/target_line.tscn")
 var plot_cooldown = 0
 
 var orders: Dictionary = {}
+var fuel: float = 0
 
 func _ready() -> void:
 	camera.make_current()
@@ -28,7 +29,7 @@ func _ready() -> void:
 	active_line.add_point(Vector2.ZERO, 1)
 	add_sibling(active_line)
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	# We should only process physics during the environment turn, not the player's planning turn
 	if Global.player_turn == true or !orders.has(1):
 		return
@@ -37,9 +38,8 @@ func _physics_process(_delta: float) -> void:
 	
 	# Move based on our speed towards order ID 1
 	var dir_normal = abs((current_target - current_origin).normalized())
-	print("dir_normal is: ", dir_normal)
-	position.x = move_toward(position.x, current_target.x, speed * dir_normal.x)
-	position.y = move_toward(position.y, current_target.y, speed * dir_normal.y)
+	position.x = move_toward(position.x, current_target.x, speed * dir_normal.x * delta)
+	position.y = move_toward(position.y, current_target.y, speed * dir_normal.y * delta)
 	
 	# Redraw order line
 	orders[1]["line"].set_point_position(0, position)
@@ -48,32 +48,43 @@ func _physics_process(_delta: float) -> void:
 	if position == current_target:
 		remove_first_order()
 	
-	# If we have no more orders, go to a new turn
-	if orders.size() == 0:
-		Global.turn_timer -= Global.DEFAULT_TURN_DURATION
+	## If we have no more orders, go to a new turn
+	#if orders.size() == 0:
+		#Global.turn_timer -= Global.DEFAULT_TURN_DURATION
 		
 	
 func _process(delta: float) -> void:
 	# We should only process here if we're in our planning turn
 	if Global.player_turn == false:
 		return
+	
 	# Move the turn planner so we can see where we're planning our turn from
-	active_indicator.position = get_global_mouse_position()
-	active_line.set_point_position(1, active_indicator.position)
-	if orders.size() == 0:
-		active_line.set_point_position(0, position)
-	else:
-		active_line.set_point_position(0, orders[orders.size()]["target"])
-		print("Orders size is: ", orders.size())
+	update_active_indicator()
+	
 	if plot_cooldown > 0:
 		plot_cooldown -= delta
-	if Input.is_action_just_pressed("plot") and plot_cooldown <= 0:
+	if Input.is_action_just_pressed("plot") and plot_cooldown <= 0 and fuel > 0:
 		plot_cooldown = DEFAULT_PLOT_COOLDOWN
 		# Run the plot order method
 		plot_order(active_indicator.position)
 	if Input.is_action_just_pressed("cancel") and plot_cooldown <= 0:
 		plot_cooldown = DEFAULT_PLOT_COOLDOWN
 		remove_last_order()
+
+func update_active_indicator() -> void:
+	var desired_target = get_global_mouse_position()
+	var desired_origin = position
+	if orders.size() != 0:
+		desired_origin = orders[orders.size()]["target"]
+	active_line.set_point_position(0, desired_origin)
+	# Set the desired target to wherever the mouse
+	
+	if desired_origin.distance_to(desired_target) > fuel:
+		#find position maximal distance away, remember A -> B = B - A. Then normalize and multiply by distance
+		desired_target = ((desired_target - desired_origin).normalized() * fuel) + desired_origin
+	active_indicator.position = desired_target
+	active_line.set_point_position(1, active_indicator.position)
+
 
 func remove_first_order() -> void:
 	# Destroy graphics for our first order
@@ -92,6 +103,10 @@ func remove_last_order() -> void:
 	if orders.size() == 0:
 		return
 	var current_id = orders.size()
+	fuel += orders[current_id]["origin"].distance_to(orders[current_id]["target"])
+	if fuel > 20:
+		active_indicator.show()
+		active_line.show()
 	orders[current_id]["indicator"].queue_free()
 	orders[current_id]["line"].queue_free()
 	orders.erase(current_id)
@@ -130,6 +145,11 @@ func plot_order(order_target: Vector2) -> int:
 	
 	print("Created order with ID: ", order_id)
 	
+	fuel -= order_origin.distance_to(order_target)
+	if fuel <= 0:
+		active_indicator.hide()
+		active_line.hide()
+	
 	return order_id	
 	
 func on_turn_end() -> void:
@@ -139,3 +159,10 @@ func on_turn_end() -> void:
 func on_turn_start() -> void:
 	active_indicator.show()
 	active_line.show()
+	fuel = speed * Global.DEFAULT_TURN_DURATION
+	if orders.size() != 0:
+		for key in orders:
+			var order_origin = orders[key]["origin"]
+			var order_target = orders[key]["target"]
+			fuel -= order_origin.distance_to(order_target)
+		
