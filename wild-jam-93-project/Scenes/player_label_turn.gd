91416e9f9f396e69962a4a686491dbcf07205541
Sadathy <118998237+Sadathy@users.noreply.github.com>
2026-05-10 10:49:33 +0100

    Minor fix
    Fixed text not updating correctly between turns.

--- wild-jam-93-project/Scenes/player_label_turn.gd
+++ wild-jam-93-project/Scenes/player_label_turn.gd
@@ -4,13 +4,13 @@ const DEFAULT_TURN_TEXT = "Your turn, press space to end"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Global.turn_ended.connect(on_turn_end)
+	Global.turn_started.connect(on_turn_start)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Global.player_turn == false:
 		text = "Time remaining: " + str(Global.turn_timer).pad_decimals(2)
-
-func on_turn_end() -> void:
+	
+func on_turn_start() -> void:
 	text = "It's your turn, plot some actions then press space to end your turn."
